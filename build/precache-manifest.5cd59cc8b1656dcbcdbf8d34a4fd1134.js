d3e01869a4148a1ee6f76b63e34502ad9f0392b8
Suelen Alves <suelen@MacBook-Pro-de-Suelen.local>
2020-09-03 12:50:03 -0300

    initial version

---
+++ build/precache-manifest.5cd59cc8b1656dcbcdbf8d34a4fd1134.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d12f6e2c83aea6abdc085adbdc563492",
+    "url": "/paleta/index.html"
+  },
+  {
+    "revision": "28127cd3e09e952c6531",
+    "url": "/paleta/static/css/main.6a379075.chunk.css"
+  },
+  {
+    "revision": "ae609f3a2b3e93a478e0",
+    "url": "/paleta/static/js/2.74bc818f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/paleta/static/js/2.74bc818f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28127cd3e09e952c6531",
+    "url": "/paleta/static/js/main.0e17cef5.chunk.js"
+  },
+  {
+    "revision": "3a989c7882d0a4af16ef",
+    "url": "/paleta/static/js/runtime-main.3f686f02.js"
+  },
+  {
+    "revision": "93d0d6f28183056dafbd6851a8e34ec4",
+    "url": "/paleta/static/media/logo-paleta.93d0d6f2.png"
+  }
+]);
---
+++ src/components/api.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class Api extends React.Component {
+    state = {
+        paleta: []
+    };
+  
+    componentDidMount() {
+        fetch('https://www.colr.org/json/colors/random/65')
+            .then(res => res.json())
+            .then(res => {
+                const paleta = res.colors.map((item) => item.hex).filter( (item) => item) 
+                this.setState({paleta});
+            });
+    }
+  
+    render() {
+        let content = '' ;
+  
+        if (this.state.paleta.length !== 0) {
+            content = (<div className="container">
+                {this.state.paleta.map(item => (
+                    <div style={{backgroundColor: "#" + item}}><p>#{item}</p></div>
+                ))}
+                </div>)
+        }
+        return (
+            <div  className="conteudo">
+                <div className="lateral"></div>
+            {content}
+            </div>
+        );
+    }
+  }
+  
+  export default Api;
